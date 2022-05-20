d504c5b480183298df5b7142cbd08c5c475f02fa
k3v <25158905+k3v53@users.noreply.github.com>
2022-05-20 11:18:59 -0300

    Working, kinda

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,8 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Injected custom css");
 		// ----------------------------------------------------------
-
+	document.querySelector(".logo").children[0].src =
+	"https://www.itse.gob.ar/wp-content/uploads/2021/04/logo-itse-nuevo-e1619458244687.png";
 	}
-	}, 10);
+	}, 100);
 });
