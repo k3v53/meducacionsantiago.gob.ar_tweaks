2a239fbd321aedf7fc508bc0850ccf3d083b308b
k3v <25158905+k3v53@users.noreply.github.com>
2022-05-27 09:06:36 -0300

    Load custom logo and minor css changes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,14 +1,23 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Injected custom css");
-		// ----------------------------------------------------------
-	document.querySelector(".logo").children[0].src =
-	"https://www.itse.gob.ar/wp-content/uploads/2021/04/logo-itse-nuevo-e1619458244687.png";
-	}
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Injected custom css");
+			// ----------------------------------------------------------
+		}
 	}, 100);
+	async function loadLogo() {
+		const logo = document.querySelector(".logo").children[0];
+		// console.log("Search found: ", logo);
+		if (logo) {
+			logo.src =
+				"https://www.itse.gob.ar/wp-content/uploads/2021/04/logo-itse-nuevo-e1619458244687.png";
+		} else {
+			setTimeout(loadLogo, 10);
+		}
+	}
+	loadLogo();
 });
