da9402ebca6f422465ff0f4688b5003c50d45edf
k3v <25158905+k3v53@users.noreply.github.com>
2022-05-27 10:19:12 -0300

    Dev (#1)
    * Working, kinda
    
    * Changed nav drawer
    
    * Modified breadcrumbs
    
    * Modified multiple icons and containers
    
    * Load custom logo and minor css changes
    
    * Changed homepage url

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,23 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Injected custom css");
-		// ----------------------------------------------------------
-
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Injected custom css");
+			// ----------------------------------------------------------
+		}
+	}, 100);
+	async function loadLogo() {
+		const logo = document.querySelector(".logo").children[0];
+		// console.log("Search found: ", logo);
+		if (logo) {
+			logo.src =
+				"https://www.itse.gob.ar/wp-content/uploads/2021/04/logo-itse-nuevo-e1619458244687.png";
+		} else {
+			setTimeout(loadLogo, 10);
+		}
 	}
-	}, 10);
+	loadLogo();
 });
